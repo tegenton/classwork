a4816601374d3bbdc4ca2fa9cb252c841a3748cb
tegenton <tegenton@ucvts.tec.nj.us>
2018-09-27 13:44:25 -0400

    Add loops
    needs testing though

--- IO test/src/Summation.java
+++ IO test/src/Summation.java
@@ -9,15 +9,20 @@ public class Summation {
     int end = in.nextInt();
     System.out.print("Loop type (1 for a while loop, 2 for a do...while loop, and 3 for a for loop): ");
     int type = in.nextInt();
+    int sum = 0;
     switch (type) {
     case 1:
-      // TODO: while
+      int i = start;
+      while (i < end) sum += i++;
       break;
     case 2:
-      // TODO: do while
+      int i = start;
+      do {
+        sum += i;
+      } while (i++ < end);
       break;
     case 3:
-      // TODO: for
+      for (int i = start; i < end; i++) sum += i;
       break;
     default:
       System.out.println("Invalid loop type");
