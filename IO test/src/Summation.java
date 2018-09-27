d76bf140a93c056a76f992cc430cda434d3b5dbf
Tim Egenton <tegenton@ucvts.tec.nj.us>
2018-09-27 14:13:20 -0400

    Fix while loop

--- IO test/src/Summation.java
+++ IO test/src/Summation.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Summation {
   public static void main(String[] args) {
-    Scanner in = new Scanner(system.in);
+    Scanner in = new Scanner(System.in);
     System.out.print("Starting value: ");
     int start = in.nextInt();
     System.out.print("Ending value: ");
@@ -12,19 +12,19 @@ public class Summation {
     in.close();
     
     int sum = 0;
+    int i = start;
+    
     switch (type) {
     case 1:
-      int i = start;
-      while (i < end) sum += i++;
+      while (i < end + 1) sum += i++;
       break;
     case 2:
-      int i = start;
       do {
         sum += i;
       } while (i++ < end);
       break;
     case 3:
-      for (int i = start; i <= end; i++) sum += i;
+      for (; i <= end; i++) sum += i;
       break;
     default:
       System.out.println("Invalid loop type");
