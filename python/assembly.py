8210dcd3fa4c2bcbf1283ec2b875dc624f637f5e
tegenton <tegenton@ucvts.tec.nj.us>
2018-12-07 16:48:54 +0000

    filter out invalid sequences

--- python/assembly.py
+++ python/assembly.py
@@ -2,8 +2,18 @@ fragments = []
 nextFragment = ''
 
 while nextFragment != 'quit':
-    nextFragment = input("Enter a fragment or quit to exit")
+    nextFragment = input("Enter a fragment or quit to exit: ").lower()
     if nextFragment != 'quit':
         fragments.append(nextFragment)
 
+print(fragments)
+
+
+for fragment in fragments[:]:
+    valid = True
+    for letter in fragment:
+        if letter not in ['a','t','c','g',]:
+            valid = False
+    if not valid:
+        fragments.remove(fragment)
 print(fragments)
